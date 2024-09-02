55f901b0347e2873284142a48cdd08b20ea84ff4
Pablo Santos <pablorasantos.dev@gmail.com>
2024-09-02 20:53:34 -0300

    feat: imagem next

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app'
+import Image from 'next/image'
 
 import { globalStyle } from '@/styles/global'
 import { Container, Header } from '@/styles/pages/app'
@@ -11,7 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logoImg.src} alt="" />
+        <Image src={logoImg} alt="" />
       </Header>
       <Component {...pageProps} />
     </Container>
